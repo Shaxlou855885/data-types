58a8e8b69dc56993c0c726f02bb425b91570edea
Shaxlou855885 <shaxloubaydullaeva4@gmail.com>
2024-11-30 16:29:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const str = prompt('string formatidagi data kiriting');
+ const num = prompt('number formatidagi data kiriting');
+ const bool = prompt('boolean formatidagi data kiriting');
+ confirm('hamma malumotlarni togri kiritingizmi?');
+
+ alert(`siz kiritgan ${str} ${typeof(str)} typega tegishli
+ siz kiritgan ${num} ${typeof(num) } typega tegishli
+ siz kiritgan ${bool} ${typeof(bool) } typega tegishli
+ `);
+ 
